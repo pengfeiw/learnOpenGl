ba09946aed83f7990617149521dfc3a8a9391460
fangyan <fangyan@qunhemail.com>
2025-06-12 17:35:43 +0800

    更新 shaders 以及 cmake 配置

--- src/ch01/helloTexture.cpp
+++ src/ch01/helloTexture.cpp
@@ -9,29 +9,6 @@ namespace hello_texture {
 	const unsigned int SCR_WIDTH = 800;
 	const unsigned int SCR_HEIGHT = 600;
 
-	const char* vertexShaderSource = "#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"layout (location = 1) in vec3 aColor;\n"
-		"layout (location = 2) in vec2 aTexCoord;\n"
-		"out vec3 ourColor;\n"
-		"out vec2 TexCoord;\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = vec4(aPos, 1.0f);\n"
-		"	ourColor = aColor;\n"
-		"   TexCoord = aTextCoord;\n"
-		"}\0";
-
-	const char* fragmentShaderSource = "#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"in vec3 ourColor;\n"
-		"in vec2 TexCoord;\n"
-		"uniform smapler2D ourTexture;\n"
-		"void main()\n"
-		"{\n"
-		"   FragColor = texture(ourTexture, TexCoord);\n"
-		"}\n\0";
-
 	int helloTexture()
 	{
 		// glfw: initialize and configure
